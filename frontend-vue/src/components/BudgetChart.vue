58b24276113dee361815bc4312fb0bbc27668bc8
yash <yash.m@peerbits.com>
2024-08-02 14:50:56 +0530

    design change

--- frontend-vue/src/components/BudgetChart.vue
+++ frontend-vue/src/components/BudgetChart.vue
@@ -1,16 +1,12 @@
 <template>
     <div class="lg:flex w-full lg:space-x-10 md:space-y-5 lg:space-y-0">
-        <div class="card lg:w-1/2 rounded-box grid h-80 md:h-96 flex-grow flex-col place-items-center">
-            <span>
-                <Bar width="650" id="my-chart-id" :options="chartOptions" :data="chartData.barChart" />
-            </span>
-            <span> Income/Expenses </span>
+        <div class="card rounded-box grid h-80 md:h-96 flex-grow place-items-center">
+            <Bar id="my-chart-id" :options="chartOptions" :data="chartData.barChart" />
+            <!-- <span> Income/Expenses </span> -->
         </div>
-        <div class="card lg:w-1/2 rounded-box grid h-80 md:h-96 flex-grow flex-col place-items-center">
-            <span>
-                <Pie width="350" :data="chartData.pieChart" :options="chartOptions" />
-            </span>
-            <span> Categories Expenses </span>
+        <div class="card rounded-box grid h-80 md:h-96 flex-grow place-items-center">
+            <Pie :data="chartData.pieChart" :options="chartOptions" />
+            <!-- <span> Categories Expenses </span> -->
         </div>
     </div>
 </template>
